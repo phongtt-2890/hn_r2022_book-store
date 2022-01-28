6a8df740da87d8cc77029734f90817996738e36c
phongtt-2890 <tran.thanh.phong@sun-asterisk.com>
2022-01-28 16:19:58 +0700

    Show product details

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,5 @@
 class BooksController < ApplicationController
+  before_action :load_book, only: %i(show)
+
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users
+    resources :books
   end
 end
